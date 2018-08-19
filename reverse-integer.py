65926a3cd6df8178efca0a1eb6620ca916bb27de
Ilya <ilyaiparshin@gmail.com>
2018-08-19 23:28:25 +0500

    done reverse

--- reverse-integer.py
+++ reverse-integer.py
@@ -4,3 +4,37 @@ class Solution:
         :type x: int
         :rtype: int
         """
+        try:
+            int(x)
+        except ValueError:
+            print("Integer expected!")
+            return 0
+
+        str_x = str(x)
+        if x < 0:
+            str_x = '-' + str_x[::-1][:-1]
+            reverse = int(str_x)
+        else:
+            str_x = str_x[::-1]
+            reverse = int(str_x)
+
+        neg_limit = -0x80000000
+        pos_limit = 0x7fffffff
+
+        if reverse < 0:
+            val = reverse & neg_limit
+            if val != neg_limit:
+                return 0
+        elif reverse == 0:
+            return reverse
+        else:
+            val = reverse & pos_limit
+            if val != reverse:
+                return 0
+
+        return reverse
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.reverse(1534236469))
